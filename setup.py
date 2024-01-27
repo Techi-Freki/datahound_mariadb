3c44ae058e1d17a18163f41d09c8cab6e2956105
D. Bryan Combs <125151537+Techi-Freki@users.noreply.github.com>
2024-01-27 00:16:34 -0500

    Updating setup.py information.

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     packages=find_packages(),
     url='https://python.dbcombs.com/simple/datahound-mariadb',
     license='MIT',
-    author='D. Bryan Combs',
-    author_email='dbcombs@gmail.com',
+    author='Techi-Freki',
+    author_email='techi-freki@proton.me',
     description='A mariadb connector for datahound.',
     install_requires=['datahound', 'mariadb'],
-    dependency_links=['https://python.dbcombs.com/simple'],
+    dependency_links=['https://github.com/Techi-Freki/datahound_mariadb'],
     entry_points={
         'datahound.connectors': ['datahound_mariadb=datahound_mariadb.connectors:MariaDbConnector']
     }
